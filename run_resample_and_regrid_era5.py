bee7a1efa8c29999536d5b85bb0a27d5f2d0bb21
Charlie Parr <cparr4@chinook03.rcs.alaska.edu>
2025-03-11 11:54:13 -0800

    move most of the args to be read from the config

--- run_resample_and_regrid_era5.py
+++ run_resample_and_regrid_era5.py
@@ -1,8 +1,11 @@
-"""Script to build a slurm file that runs resample_and_regrid_era5.py.
+"""Script to build a slurm file that runs the worker script resample_and_regrid_era5.py.
 
 
 example usage:
-    python /beegfs/CMIP6/kmredilla/repos/cmip6-utils/downscaling/run_resample_and_regrid_era5.py --conda_env_name snap-geo --wrf_era5_directory /beegfs/CMIP6/wrf_era5/04km --output_directory /beegfs/CMIP6/kmredilla/daily_era5_4km_3338/netcdf --slurm_directory /beegfs/CMIP6/kmredilla/daily_era5_4km_3338/netcdf --geo_file /beegfs/CMIP6/wrf_era5/geo_em.d02.nc --start_year 1965 --end_year 2022
+    python /beegfs/CMIP6/$USER/repos/cmip6-utils/downscaling/run_resample_and_regrid_era5.py
+        --conda_env_name snap-geo
+        --output_directory /beegfs/CMIP6/$USER/daily_era5_4km_3338/netcdf
+        --slurm_directory /beegfs/CMIP6/$USER/daily_era5_4km_3338/netcdf
 """
 
 import argparse
@@ -10,6 +13,7 @@ import logging
 import subprocess
 from pathlib import Path
 
+from config import config
 
 logging.basicConfig(
     level=logging.INFO,
@@ -21,9 +25,8 @@ logging.basicConfig(
 def submit_sbatch(sbatch_fp):
     """Submit a script to slurm via sbatch.
 
-    Parameters
-    ----------
-    sbatch_fp : pathlib.PosixPath
+    Args:
+        sbatch_fp : pathlib.PosixPath
         path to .slurm script to submit
 
     Returns
@@ -82,20 +85,10 @@ def parse_args():
     -------
     conda_env_name : str
         Name of conda environment to activate
-    wrf_era5_directory : str
-        Path to directory where WRF-downscaled ERA5 data is stored
     output_directory : str
         Path to directory where resampled and reprojected ERA5 data will be written
     slurm_directory : str
         Path to directory for writing slurm files
-    geo_file : str
-        Path to WRF geo_em file for projection information
-    start_year : str
-        Start year for processing
-    end_year : str
-        End year for processing
-    no_clobber : bool
-        Do not overwrite existing files in output_directory
     """
 
     parser = argparse.ArgumentParser(description=__doc__)
@@ -103,12 +96,6 @@ def parse_args():
         "--conda_env_name",
         type=str,
         help="Name of conda environment to activate",
-        required=True,
-    )
-    parser.add_argument(
-        "--wrf_era5_directory",
-        type=str,
-        help="Path to directory where WRF-downscaled ERA5 data is stored",
         required=True,
     )
     parser.add_argument(
@@ -123,58 +110,25 @@ def parse_args():
         help="Path to directory for writing slurm files",
         required=True,
     )
-    parser.add_argument(
-        "--geo_file",
-        type=str,
-        help="Path to WRF geo_em file for projection information",
-        required=True,
-    )
-    parser.add_argument(
-        "--start_year",
-        type=str,
-        help="Start year for processing",
-        required=True,
-    )
-    parser.add_argument(
-        "--end_year",
-        type=str,
-        help="End year for processing",
-        required=True,
-    )
-    parser.add_argument(
-        "--no_clobber",
-        action="store_true",
-        default=False,
-        help="Do not overwrite existing files in output_directory",
-    )
+
     args = parser.parse_args()
 
     return (
-        # Path(args.conda_init_script),
         args.conda_env_name,
-        Path(args.wrf_era5_directory),
         Path(args.output_directory),
         Path(args.slurm_directory),
-        Path(args.geo_file),
-        args.start_year,
-        args.end_year,
-        args.no_clobber,
     )
 
 
 if __name__ == "__main__":
 
     (
-        # conda_init_script,
         conda_env_name,
-        wrf_era5_directory,
         output_directory,
         slurm_directory,
-        geo_file,
-        start_year,
-        end_year,
-        no_clobber,
     ) = parse_args()
+
+    wrf_era5_directory = config.INPUT_DIR
 
     output_directory.mkdir(exist_ok=True)
     slurm_directory.mkdir(exist_ok=True)
@@ -184,8 +138,10 @@ if __name__ == "__main__":
     process_era5_sbatch_out_file = str(process_era5_sbatch_file).replace(
         ".slurm", "_%j.out"
     )
-
     config_file = slurm_directory.joinpath("slurm_array_config.txt")
+
+    start_year = config.START_YEAR
+    end_year = config.END_YEAR
     write_config_file(config_file, start_year, end_year)
     array_range = get_array_range(start_year, end_year)
 
@@ -199,16 +155,18 @@ if __name__ == "__main__":
         f"#SBATCH --time=08:00:00\n"
         f"#SBATCH --output {process_era5_sbatch_out_file}\n"
         "echo Start slurm && date\n"
-        # this should work to initialize conda without init script
+        # this should work to initialize conda without a conda init script
         'eval "$($HOME/miniconda3/bin/conda shell.bash hook)"\n'
         f"conda activate {conda_env_name}\n"
         f"config={config_file}\n"
         # Extract the year to process for the current $SLURM_ARRAY_TASK_ID
         "year=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $2}' $config)\n"
         f"python resample_and_regrid_era5.py --era5_dir {wrf_era5_directory} "
-        f"--output_dir {output_directory} --year $year --geo_file {geo_file} "
+        f"--output_dir {output_directory} --year $year --geo_file {config.GEO_EM_FILE} "
     )
-    if no_clobber:
+    print(config.OVERWRITE)
+    print(type(config.OVERWRITE))
+    if not config.OVERWRITE:
         sbatch_text += "--no_clobber"
 
     # save the sbatch text as a new slurm file in the repo directory
